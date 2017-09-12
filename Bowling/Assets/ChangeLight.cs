a0273f58cbc554f6e4bb4b3a5370abfa281326dd
Łukasz Berwid <lucky125111@gmail.com>
2017-09-12 20:08:16 +0200

    added fps counter, and light toggle

---
+++ Bowling/Assets/ChangeLight.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeLight : MonoBehaviour {
+
+    public Light BallLight { get; set; }
+    public Light PinsLight { get; set; }
+    public Light PointStraightUp { get; set; }
+    public Light PointRotated { get; set; }
+
+	// Use this for initialization
+	void Start () {
+	    BallLight = GameObject.Find("BallLight").GetComponent<Light>();
+	    PinsLight = GameObject.Find("PinsLight").GetComponent<Light>();
+	    PointStraightUp = GameObject.Find("PointStraightUp").GetComponent<Light>();
+	    PointRotated = GameObject.Find("PointRotated").GetComponent<Light>();
+
+	    BallLight.enabled = true;
+	    PinsLight.enabled = true;
+	    PointStraightUp.enabled = false;
+	    PointRotated.enabled = false;
+
+	}
+
+    // Update is called once per frame
+    void FixedUpdate () {
+        if (Input.GetKey(KeyCode.Q))
+        {
+            BallLight.enabled = !BallLight.enabled;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            PinsLight.enabled = !PinsLight.enabled;
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            PointStraightUp.enabled = !PointStraightUp.enabled;
+        }
+        if (Input.GetKey(KeyCode.R))
+        {
+            PointRotated.enabled = !PointRotated.enabled;
+        }
+    }
+}
---
+++ Bowling/Assets/FpsOnCanvas.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FpsOnCanvas : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+	    var txt = GameObject.Find("FPSCounter").GetComponent<Text>();
+	    txt.text = "FPS: " + (int)(1f / Time.unscaledDeltaTime);
+	}
+}
