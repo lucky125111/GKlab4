4ca1e20982e4fe3442bf67f049620f00dcf1371d
Łukasz Berwid <lucky125111@gmail.com>
2017-09-14 22:43:14 +0200

    change shader by pressing keys (qwertyu)

--- Bowling/Assets/ShaderChanger.cs
+++ Bowling/Assets/ShaderChanger.cs
@@ -7,13 +7,83 @@ public class ShaderChanger : MonoBehaviour
     public Material m1;
 
     public Material m2;
-	// Use this for initialization
-	void Start () {
+
+    public Shader ConstPhong;
+    public Shader ConstBlinn;
+    public Shader GourardPhong;
+    public Shader GourardBlinn;
+    public Shader Phong;
+    public Shader PhongBlinn;
+    public Shader Static;
+    public Shader Triangle;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+	    if (Input.GetKey(KeyCode.Q))
+	    {
+	        m1.SetFloat("_Shininess", 3);
+	        m2.SetFloat("_Shininess", 3);
+
+            m1.shader = ConstPhong;
+	        m2.shader = ConstPhong;
+        }
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        m1.SetFloat("_Shininess", 10);
+	        m2.SetFloat("_Shininess", 10);
+
+            m1.shader = ConstBlinn;
+	        m2.shader = ConstBlinn;
+	    }
+	    if (Input.GetKey(KeyCode.E))
+	    {
+	        m1.SetFloat("_Shininess", 3);
+	        m2.SetFloat("_Shininess", 3);
+
+            m1.shader = GourardPhong;
+	        m2.shader = GourardPhong;
+	    }
+	    if (Input.GetKey(KeyCode.R))
+	    {
+	        m1.SetFloat("_Shininess", 10);
+	        m2.SetFloat("_Shininess", 10);
+
+            m1.shader = GourardBlinn;
+	        m2.shader = GourardBlinn;
+	    }
+	    if (Input.GetKey(KeyCode.T))
+	    {
+	        m1.SetFloat("_Shininess", 3);
+	        m2.SetFloat("_Shininess", 3);
+
+            m1.shader = Phong;
+	        m2.shader = Phong;
+	    }
+	    if (Input.GetKey(KeyCode.Y))
+	    {
+	        m1.SetFloat("_Shininess", 10);
+	        m2.SetFloat("_Shininess", 10);
+
+            m1.shader = PhongBlinn;
+	        m2.shader = PhongBlinn;
+        }
+	    if (Input.GetKey(KeyCode.U))
+	    {
+
+	        m1.shader = Static;
+	        m2.shader = Static;
+	    }
+	    if (Input.GetKey(KeyCode.I))
+	    {
+	        m1.SetFloat("_Shininess", 10);
+	        m2.SetFloat("_Shininess", 10);
+
+            m1.shader = Triangle;
+	        m2.shader = Triangle;
+	    }
+    }
 }
